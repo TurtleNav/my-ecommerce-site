ba54e3f4c47ff9c3fb915e66ea5cad8eef45138f
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-13 23:56:51 -0500

    added trivial relationships as requested in index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {foreignKey: "category_id"});
 
-// Categories have many Products
+// Categories have many Products - hasMany
+// use CASCADE to delete any instances of that category in
+// Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+});
 
 module.exports = {
   Product,
